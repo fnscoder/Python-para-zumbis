068a9127031f4b027081035336f8a696ac361e76
outrofelipe <felipensouza@live.com>
2015-04-28 04:20:37 -0300

    lista 3b exercicio 1 atualizado

--- lista-3b/01_triangular.py
+++ lista-3b/01_triangular.py
@@ -1,4 +1,5 @@
 '''
+Lista 3b - Exercício 1
 Dizemos que um número natural é triangular se ele é produto de três números naturais
 consecutivos. Exemplo: 120 é triangular, pois 4.5.6 = 120. Dado um inteiro não-negativo n,
 verificar se n é triangular.
